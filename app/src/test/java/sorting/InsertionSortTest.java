ce27e7e7a591b7faee5135109161608cd30982e2
albfor <96afo02@gmail.com>
2021-07-21 15:00:21 +0200

    Added more demanding tests

--- app/src/test/java/sorting/InsertionSortTest.java
+++ app/src/test/java/sorting/InsertionSortTest.java
@@ -1,12 +1,41 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class InsertionSortTest {
+    private int[] sorted;
+    private int[] shuffled;
+
+    private void createInput(int size) {
+        ArrayList<Integer> integers = new ArrayList<>(size);
+        for (int i = 1; i < size; i++) {
+            integers.add(i);
+        }
+        sorted = integers.stream().mapToInt(i -> i).toArray();
+        Collections.shuffle(integers);
+        shuffled = integers.stream().mapToInt(i -> i).toArray();
+    }
+
     @Test
     void simpleSortTest() {
         int[] array = new int[] {5,2,6,3,1,4};
         InsertionSort.sort(array);
         assertArrayEquals(new int[]{1,2,3,4,5,6}, array);
+    }
+
+    @Test
+    void advancedTest() {
+        createInput(10_000);
+        InsertionSort.sort(shuffled);
+        assertArrayEquals(sorted, shuffled);
+    }
+
+    @Test 
+    void seriousTest() {
+        createInput(1_000_000);
+        InsertionSort.sort(shuffled);
+        assertArrayEquals(sorted, shuffled);
     }
 }
 
