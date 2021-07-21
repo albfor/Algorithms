d63a35b742d64393a49e94f7c1181495ce9c22b6
albfor <96afo02@gmail.com>
2021-07-21 15:59:43 +0200

    Added MergeSort with tests

---
+++ app/src/main/java/sorting/MergeSort.java
@@ -0,0 +1,51 @@
+public class MergeSort {
+    public static void sort(int[] input, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(input, l, m);
+            sort(input, m + 1, r);
+            merge(input, l, m, r);
+        }
+    }
+
+    private static void merge(int[] input, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = createTempArray(input, l, n1);
+        int[] R = createTempArray(input, m + 1, n2);
+        int i = 0, j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                input[k] = L[i];
+                i++;
+            } else {
+                input[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            input[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            input[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static int[] createTempArray(int[] input, int start, int size) {
+        int[] temp = new int[size];
+        for (int i = 0; i < size; i++) {
+            temp[i] = input[start + i];
+        }
+        return temp;
+    }
+}
+
---
+++ app/src/test/java/sorting/MergeSortTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class MergeSortTest {
+    private int[] expected;
+    private int[] actual;
+
+    private void createInput(int size) {
+        ArrayList<Integer> integers = new ArrayList<>(size);
+        for (int i = 1; i < size; i++) {
+            integers.add(i);
+        }
+        expected = integers.stream().mapToInt(i -> i).toArray();
+        Collections.shuffle(integers);
+        actual = integers.stream().mapToInt(i -> i).toArray();
+    }
+
+    @Test
+    void simpleMergeSortTest() {
+        createInput(10);        
+        MergeSort.sort(actual, 0, actual.length - 1);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void advancedMergeSortTest() {
+        createInput(10_000);        
+        MergeSort.sort(actual, 0, actual.length - 1);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void seriousMergeSortTest() {
+        createInput(1_000_000);        
+        MergeSort.sort(actual, 0, actual.length - 1);
+        assertArrayEquals(expected, actual);
+    }
+}
