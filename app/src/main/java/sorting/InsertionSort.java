eb7f1cdc8810448d4f12e230dd9ca5e572b7cddc
albfor <96afo02@gmail.com>
2021-07-21 13:45:09 +0200

    Added InsertionSort

---
+++ app/src/main/java/sorting/InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+    public static void sort(int[] input) {
+        for (int i = 1; i < input.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
